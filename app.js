cddd40ade5aa75efff8f873194e7683c79047983
ronald1101 <dsouzaronald13@gmail.com>
2019-01-13 10:05:00 +0400

    adde cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	// Website you wish to allow to connect
-	// res.setHeader('Access-Control-Allow-Origin', 'http://10.200.197.65:4200');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
 	// Request methods you wish to allow
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -23,7 +23,7 @@ app.use('/', function (req, res, next) {
 
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
+	// res.setHeader('Access-Control-Allow-Credentials', true);
 
 	next();
 });
